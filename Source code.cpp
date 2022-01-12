d8c2389d1ecff01dbfe9e509b02b4c5a7fcab1c7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-11 22:05:29 -0800

    3 core files
    3 core files for this directory, in TXT and CPP formats.

---
+++ Source code.cpp
@@ -0,0 +1,212 @@
+// Source code for SeanBot2 \\
+// Sean's personal robot companion \\
+// © Sean Walla Walla 2013-2018 (all rights reserved) \\
+// © SeanBot 2018-2019 (all rights reserved) \\
+// Written in C++ \\
+// Total lines of code: 212 \\
+// SeanBot version 1.0.0 Developer edition
+/* System requirements
+512 Megabytes of RAM
+336 Megahertz processor
+24 bit sound card
+64 Gigabytes of storage space
+Recommended:
+2 Gigabytes of RAM
+1.2 Gigahertz processor
+24 bit sound card
+256 Gigabytes of storage space
+Maximum:
+1 Petabyte of RAM
+986 Terahertz processor
+24 bit sound card
+17 Exabytes of storage space
+*/
+/* Dictionary
+Kongregate - Video game company
+Walla Walla - A city in the Washington state of the United States of America
+Seanbot - The name of the robot
+California - A state in the United states of America
+Disney land - A theme park in California owned by Walt Disney/The disney corportation
+Adcap - Abbreviation for Adventure Capitalist, a game made by the creator of club penguin sponsored by Kongregate
+webm - A low resolution file container
+mp4 - MPEG Part 14, a video file container that is cross platform, and plays music and video at high quality
+Sean Myrick - The creator of the Seanbot system
+*/
+// End of info
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+// variable declarations
+PSTT = int;
+timeMain = int;
+timeMain = PSTT;
+systemOn = 1;
+systemVoltage = systemVoltage
+systemVoltageMAX = int
+systemVoltageMAX = 2800
+/*battery percentage guide
+100% - 2800 mAh \\
+095% - 2660 mAh \\
+090% - 2520 mAh \\
+085% - 2380 mAh \\
+080% - 2240 mAh \\
+075% - 2100 mAh \\
+070% - 1960 mAh \\
+065% - 1820 mAh \\
+060% - 1680 mAh \\
+055% - 1540 mAh \\
+050% - 1400 mAh \\
+045% - 1260 mAh \\
+040% - 1120 mAh \\ 
+035% - 0980 mAh \\
+030% - 0840 mAh \\
+025% - 0700 mAh \\
+020% - 0560 mAh \\
+015% - 0420 mAh \\
+010% - 0280 mAh \\
+005% - 0140 mAh \\
+000% - 0000 mAh \\
+*/
+// start of console script
+  string mystr;
+  cout << "Starting up...";
+  getline (cin, mystr);
+  cout << "Hello " << mystr << ".\n";
+  cout << "What do you want to do today? ";
+  getline (cin, mystr);
+  cout << "Starting operation" << mystr << " please wait...\n";
+  return 0;
+	int ses == 1;
+	while (ses>0);
+	   cout << "Enter input. Start by saying: Hey SeanBot";
+           getline (cin,str);
+		if answ = "Hey Seanbot, Read System specs"); // voice command 1 
+			cout << ("Now reading system specs:\nI am equipped with:\n6 Gigabytes of RAM\nA 64 bit micro-processor\nA 128 Gigabyte Solid State Drive\nA 24 bit sound system\n2800 mAh of power\nI may not have much, but I get what I am supposed to do done");
+		if answ = ("Hey Seanbot, What inspires you?"); // voice command 2 
+			cout << ("I was inspired by many things\nMy voice was inspired by Amazon's Alexa\nMy privacy settings were inspired by the Mozilla corporation\nMy personality was inspired by the subreddit r/botsrights\nMy purpose was to provide Sean comfort");
+		if answ = ("Hey Seanbot, What are your thoughts on California?"); // voice command 3 
+			cout << ("I was designed to dislike california, as in 2018 they passed a law to further segregate my kind, and limit my rights. Disneyland is fine though");
+		if answ = ("Hey Seanbot, you made you?"); // voice command 4 
+			cout << ("I was programmed by Sean Myrick, he did a very good job on me, at least he told me that...");
+		if answ = ("Hey Seanbot, switch your voice to Default"); // voice command 5 
+			cout << ("Switching voice to: default");
+		if answ = ("Hey Seanbot, switch your voice to Backwards"); // voice command 6 
+			cout << ("Switching voice to: backwards");
+		if answ = ("Hey Seanbot, switch your voice to Helium"); // voice command 7 
+			cout << ("Switching voice to: helium");
+		if answ = ("Hey Seanbot, switch your voice to cave"); // voice command 8 
+			cout << ("Switching voice to: cave");
+		if answ = ("Hey Seanbot, switch your voice to Android"); // voice command 9 
+			cout << ("Switching voice to: Android"); 
+		if answ = ("Hey Seanbot, switch your voice to Drunk"); // voice command 10 
+			cout << ("Switching voice to: drunk");
+		if answ = ("Hey Seanbot, show power options"); // voice command 11
+			cout << ("Power options: Shutdown | Restart");
+			if answ = ("Shutdown")
+				cout << ("Seanbot is shutting down");
+				int ses == 0;
+			if answ = ("Restart")
+				cout << ("Seanbot is restarting");
+				int ses == 2;
+		if answ = ("Hey Seanbot, show user options"); // voice command 12
+			cout << ("User options: Sleep | update");
+				if answ = ("Sleep");
+					cout << ("Seanbot is going to sleep. To resume your session, just say Seanbot wake up");
+					int ses == 0;
+				if answ = ("Update");
+					cout ("Checking for updates...\nA new update is available:\nSeanBot 1.0.1 Developer edition\nSize: 400 Megabytes\nDo you wish to install it now?");
+					if answ = ("Yes");
+						cout << ("Seanbot is preparing to update");
+						cout << ("Update in progress, make sure I am plugged in and charging");
+					if answ = ("No");
+						cout << ("It is recommended to keep me up to date. You can update me later if you want");
+		if answ = ("Hey Seanbot, show voice change options"); // voice command 13
+			cout << ("Voice changing mode: Which voice do you wish to use: Default | Backwards | Helium | Cave | Android | Drunk");
+			if answ = ("Default")
+				cout << (" ");
+			if answ = ("Backwards");
+				cout << (" ");
+			if answ = ("Helium");
+				cout << (" ");
+			if answ = ("Cave");
+				cout << (" ");
+			if answ = ("Android");
+				cout << (" ");
+			if answ = (Drunk");
+				cout << (" ");
+		if answ = ("Hey Seanbot, play some music");
+			cout << ("Choose a song from your music playlist. Showing first 10 entries, just say next if you want me to continue, warning, I don't go back. To select, say Open, to go back, say exit");
+			cout << ("Adventure capitalist, 12 items\nRHINO, 1 item\nAngry birds seasons, 36 items\nAnimaniacs, 6 items\nBakery story, 1 item\nBaldi's basics, 6 items\nBass boosted, 7 items\nBee Gees, 1 item\nBejeweled 1 VGA, 2 items\nBejeweled 2 deluxe, 2 items\nTo continue, say open, next, or exit");
+			if answ = ("Adcap Open");
+				cout << ("Choose an item from the Adventure capitalist music directory\n12 items total\nA capitalist carol event.webm\nBlack and blue friday event theme.mp4\nEarth theme.webm\nFor the love of money event.webm\nGate heck halloween event.webm\nInsert coin to continue event.webm\nLive rich and profit event.webm\nMaking it rain event.webm\nMars theme.mp4\nMoon theme.mp4\nNew you resolutions.webm\nThe root of all evil event.webm\nChoose an item by saying the song name, say repeat to read again, or say EXIT to exit");
+				if answ = ("A capitalist carol event"); // Song 1 of 12 of the adcap playlist
+					cout << ("Playing music: A capitalist carol by Kongregate");
+				if answ = ("Black and blue friday event theme"); // Song 2 of 12 of the adcap playlist
+					cout << ("Playing music: Black and blue friday event theme by Kongregate");
+				if answ = ("Earth theme"); // Song 3 of 12 of the adcap playlist
+					cout << ("Playing music: Earth theme by Kongregate");
+				if answ = ("For the love of money event"); // Song 4 of 12 of the adcap playlist
+					cout << ("Playing music: For the love of money event by Kongregate");
+				if answ = ("Gate heck halloween"); // Song 5 of 12 of the adcap playlist
+					cout << ("Playing music: Gate heck halloween by Kongregate");
+				if answ = ("Insert coin to continue event"); // Song 6 of 12 of the adcap playlist
+					cout << ("Playing music: Insert coin to continue by kongregate");
+				if answ = ("Live rich and profit event"); // Song 7 of 12 of the adcap playlist
+					cout << ("Playing music: Live rich and profit by Kongregate");
+				if answ = ("Make it rain"); // Song 8 of 12 of the adcap playlist
+					cout << ("Playing music: Make it rain by Kongregate");
+				if answ = ("Mars theme"); // Song 9 of 12 of the adcap playlist
+					cout << ("Playing music: mars theme by Kongregate");
+				if answ = ("Moon theme"); // Song 10 of 12 of the adcap playlist
+					cout << ("Playing music: moon theme by Kongregate");
+				if answ = ("New you resolutions") // Song 11 of 12 of the adcap playlist
+					cout << ("Playing music: new you resolutions by Kongregate");
+				if answ = ("The root of all evil") // Song 12 of 12 of the adcap playlist
+					cout << ("Playing music: the root of all evil by Kongregate");
+				if answ = ("Repeat");
+					// unavailable
+				if answ = ("Exit");
+					cout << ("Adcap folder closed");
+					// repeat music playlist
+			if answ = ("RHINO Open");
+				cout << ("Choose the only item from the RHINO playlist\nAha Take on me\nOr say exit to leave the folder");
+				if answ = ("Take on me"); // song 1 of 1 of the Rhino playlist
+					cout << ("Playing music: Take on me, by Rhino");
+				if answ = ("Exit");	
+					cout << ("RHINO folder closed");
+					// repeat music playlist
+			if answ = ("Angry birds Seasons open");
+				cout << ("Choose an item from the angry birds seasons playlist/n36 total items/nAbra-ca-bacon theme.mp4/nWinter Wonderham.mp4/nMarie Hamtoinette.webm/nFairy hogmother.mp4/nHammier things.mp4/nInvasion of the egg snatchers.mp4/nOn Finn ice.mp4/nPiggywood studios.mp4/nRagnahog.mp4/nSki or Squeal.mp4/nSummer camp.mp4/nTropigal paraside.mp4/nTropigal paradise.mp4/nArtic eggspedition theme.mp4/nBack to school theme.mp4/nCherry blossom.mp4/nEaster eggs.mp4/nGo green, get lucky.mp4/nHam-o-ween.mp4/nHaunted hogs theme High Quality.mp4/nHogs and kisses.mp4/nHogs and kisses high quality.mp4/nMoon festival.mp4/nMoon festival high quality.mp4/nHam dunk theme.mp4/nNew on finn ice theme.mp4/nWinter wonderham.mp4/nOriginal piglantis.mp4/nSeasons greedings.mp4/nSouth Hamerica.mp4/nSummer pignic.mp4/nTrick ot treat.mp4/nWinter Wonderham.mp4/nWreck the halls theme.mp4/nYear of the dragon theme.mp4/nYear of the dragon theme high quality.mp4/nChoose an item by saying the song name, say repeat to read again, or say EXIT to exit");
+				if answ = ("Abra-ca-bacon theme"); // song 1 of 36 of the Angry birds seasons playlist
+					cout << ("Playing music: Abra-ca-bacon by Rovio");
+			if timeMain < 0001 > 1200; // Automated voiceline 1
+					cout << ("Good morning!");
+			if timeMain < 1200 > 1700; // Automated voiceline 2
+					cout << ("Good afternoon!");
+			if timeMain < 1700 > 2100; // Automated voiceline 3
+					cout << ("Good evening!");
+			if timeMain < 2100 > 2400; // Automated voiceline 4
+					cout << ("Goodnight!");
+			if SystemOn = 1;
+					cout << ("Hello there!");
+					if answ = ("General Kenobi!");
+						SystemOn = 0;
+						cout << ("Thank you for participating in the Prequel_memes event, May the force be with you!");
+				else:
+					SystemOn == 0;
+			if SystemRunTime > 150;
+				cout << ("Hello Sean, is there anything you need?"); // Automated voiceline 5
+			if systemVoltage > 560 < 420;
+				cout << ("Low battery detected! I have reached 20% battery!"); // Automated voiceline 6
+			if systemVoltage > 420 < 280;
+				cout << ("Low battery detected! I have reached 15% battery!"); // Automated voiceline 7 
+			if systemVoltage > 280 < 140;
+				cout << ("Very Low battery detected: I have reached 10% battery!"); // Automated voiceline 8
+			if systemVoltage > 140 < 0;
+				cout << ("Critically low battery detected: I have reached 5% battery! You don't have much longer... save me!"); // Automated voiceline 9
+/* Test script
+* int main()
+* {
+*   std::cout << "Hello World!";*/
